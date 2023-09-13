123b88bc418c19615abd49d59834670abb89446b
Maxim Sukhochev <sukho4evmaxim@gmail.com>
2023-09-13 04:12:08 +0300

    Added simple keyboard

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, Markup } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command('start', ctx => {
     console.log(ctx.from)
     if (ctx.from.language_code == 'ru') {
-        bot.telegram.sendMessage(ctx.chat.id, 'Привет! Если ты видишь этот текст, значит бот работает.', {})
+        ctx.reply('Привет! Если ты видишь этот текст, значит бот работает.');
     } else {
-        bot.telegram.sendMessage(ctx.chat.id, 'Hello! If you can see this text, the bot is working.', {})
+        ctx.reply('Hello! If you can see this text, the bot is working.');
     }
+    ctx.reply(
+        'Which pill?',
+        Markup.keyboard(['Blue', 'Red']
+        ));
+})
+
+bot.hears('Blue', ctx => {
+    ctx.reply(`You've chosen blue pill.`);
+})
+
+bot.hears('Red', ctx => {
+    ctx.reply(`You've chosen red pill.`);
 })
 
 bot.launch();
